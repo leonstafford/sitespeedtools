fe7606d050e3244245fe203698396a9c54e8a4b0
Leon Stafford <me@ljs.dev>
2023-04-09 10:14:02 +1000

    adjust test provisioning

--- launch-and-test-wp-site.sh
+++ launch-and-test-wp-site.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-# build our WordPress, MySQL and Playwright containers
+echo "build our WordPress, MySQL and Playwright containers"
 ./build-containers.sh
-# launch the WordPress and MySQL containers
+echo "launch the WordPress and MySQL containers"
 docker-compose up -d db wordpress
-# use Playwright to complete WordPress installation
+echo "use Playwright to complete WordPress installation"
 docker-compose run --rm tester node setup-wp.js
 
 # In order to execute WP-CLI commands from Playwright, we'll setup 
 # SSH connectivity from "tester" to "wordpress"
 
-# generate public key on testing container (don't rm the container!)
+echo "Generating public key on testing container (don't remove the container!)"
 docker-compose run tester sh -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
 
-# save pub key from tester container to wordpress' authorized_keys
+echo "Saving public key from tester container as variable"
 TESTER_PUB_KEY=$(docker-compose exec tester sh -c 'cat ~/.ssh/id_rsa.pub')
 
-# confirm public key was captured
-cat "$TESTER_KEY"
+echo "Confirming public key was captured"
+echo "$TESTER_PUB_KEY"
 
-# enable SSH server on wordpress container
-docker-compose run wordpress bash -c "apt-get update && apt-get install -y openssh-server"
+echo "Enabling SSH server on WordPress container"
+docker-compose run wordpress bash -c "apt-get update -q && apt-get install -yq openssh-server"
 # all "tester" container to SSH into the "wordpress" container via public key authentication
-docker-compose run wordpress bash -c "mkdir ~/.ssh && tee $TESTER_KEY > ~/.ssh/authorized_keys"
+docker-compose run wordpress bash -c "mkdir -p ~/.ssh && echo '$TESTER_PUB_KEY' >> ~/.ssh/authorized_keys"
 # start the SSH service
 docker-compose run wordpress bash -c "service start ssh"
 
 # verify SSH connectivity between containers
 docker-compose run tester sh -c 'ssh root@wordpress -c "hostname"'
+
+
+# debug
+exit 1
 
 
 # install WP-CLI so we can use it via ssh2
