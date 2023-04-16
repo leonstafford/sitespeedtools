69e016807e04bb92cf71df5d2844f003c99e4853
Leon Stafford <me@ljs.dev>
2023-04-16 16:07:16 +1000

    add PHPMyAdmin to docker-compose

--- launch-wp-test-site.sh
+++ launch-wp-test-site.sh
@@ -5,7 +5,7 @@ set -e
 echo "build our WordPress, MySQL and Playwright containers"
 ./build-containers.sh
 echo "launching just the WordPress, MySQL and WP-CLI containers"
-docker-compose up -d wordpress db wpcli 
+docker-compose up -d wordpress db wpcli pma
 echo "use Playwright to complete WordPress installation"
 docker-compose build tester
 
